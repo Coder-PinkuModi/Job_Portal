2bb557a3a72f50e42483e4904d909c489d8f098f
Pinku Modi <152652870+Coder-PinkuModi@users.noreply.github.com>
2024-10-14 20:49:19 +0530

    added case-sensitive-plugin for vercel deployement

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,12 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import CaseSensitivePathsPlugin from 'case-sensitive-paths-webpack-plugin';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    new CaseSensitivePathsPlugin()
+  ],
   resolve: {
     alias: {
       '@': '/src', // Ensure alias is defined here too
